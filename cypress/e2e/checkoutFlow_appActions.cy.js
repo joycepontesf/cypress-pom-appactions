289e5511c1ea06c9bf0609b784ceb7d03fa0f1e9
Joyce Pontes <joycepontesf@gmail.com>
2024-09-09 23:03:02 -0300

    Adição de imports e inclusão de assert

--- cypress/e2e/checkoutFlow_appActions.cy.js
+++ cypress/e2e/checkoutFlow_appActions.cy.js
@@ -1,17 +1,23 @@
 /// <reference types="cypress" />
 
+import { elements } from '../support/elements';
+
 describe('', () => {
 
     beforeEach(() => {
-        cy.setCookie('ebacStoreVersion', v2, { domain: 'lojaebac.ebaconline.art.br' })
+        cy.setCookie('ebacStoreVersion', 'v2', { domain: 'lojaebac.ebaconline.art.br' })
         cy.visit('/')
     });
 
     it('Complete purchase successfully', () => {
+        cy.login()
+        cy.addProducts()
 
+        cy.get(elements.item).should('exist')
     });
 
     afterEach(() => {
         cy.clearAllCookies
     });
+
 });
