b472940be89b3a4bc010c1a5d2ca451599460396
Joyce Pontes <joycepontesf@gmail.com>
2024-09-09 23:00:22 -0300

    Reorganização arquivo elements.js

---
+++ cypress/support/elements.js
@@ -0,0 +1,23 @@
+export const elements = {
+
+    txtEmail: '[data-testid="email"]',
+    btnLogin: '[data-testid="btnLogin"]',
+    btnProfile: '[href="/Tab/Account"]',
+    btnOrder: '[href="/Tab/Order"]',
+    btnsignUp: '[data-testid="signUp"]',
+    txtFirstName: '[data-testid="firstName"]',
+    txtLastName: '[data-testid="lastName"]',
+    txtPhoneNumber: '[data-testid="phone"]',
+    txtEmailCreation: "[placeholder='Email Address']",
+    txtPassword: '[data-testid="password"]',
+    txtConfirmPassword: '[data-testid="repassword"]',
+    btnCreate: '[data-testid="create"]',
+    username: '[data-testid="CustomerName"]',
+    product: '[data-testid="productDetails"]',
+    btnContinueToCheckout: '[data-testid="selectAddressOrContinueToPayment"]',
+    btnCheckout: '[data-testid="completeCheckout"]',
+    btnPurchase: '[data-testid="addToCart"]',
+    item: '[data-testid="item-0"]',
+    processingOrders: '[data-testid="Placed"]'
+
+}
