fa2c4b3b8d024ead8a2ba367ec2c084f1d7de51c
Joyce Pontes <joycepontesf@gmail.com>
2024-09-09 23:01:21 -0300

    Atualizações no arquivo cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
   e2e: {
+
+    baseUrl: "http://lojaebac.ebaconline.art.br/",
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
