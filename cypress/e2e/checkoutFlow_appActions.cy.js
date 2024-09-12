120170723e852599e8da553ac1581a9a72c2d6f8
Joyce Pontes <joycepontesf@gmail.com>
2024-09-11 21:04:30 -0300

    Criação de novos testes, remoção de item e compra sem login

--- cypress/e2e/checkoutFlow_appActions.cy.js
+++ cypress/e2e/checkoutFlow_appActions.cy.js
@@ -7,18 +7,33 @@ describe('', () => {
     beforeEach(() => {
         cy.setCookie('ebacStoreVersion', 'v2', { domain: 'lojaebac.ebaconline.art.br' })
         cy.visit('/')
-        cy.login()
     })
 
     it('Complete purchase successfully', () => {
-        cy.addProducts()
+        cy.login()
+        cy.addProduct()
+        cy.completeOrder()
         cy.accessOrders()
-
+        
         cy.get(elements.item).should('exist')
+
+        cy.cancelOrder()
+    })
+
+    it('Remove item from the cart', () => {
+        cy.addProduct()
+        cy.removeItem()
+
+        cy.get(elements.msgCartEmpty).should('exist')
+    })
+
+    it('Buying without login', () => {
+        cy.addProduct()
+
+        cy.get(elements.btnContinueToCheckout).should('have.css', 'pointer-events', 'none');
     })
 
     afterEach(() => {
-        cy.cancelOrder()
         cy.clearAllCookies
     })
 
