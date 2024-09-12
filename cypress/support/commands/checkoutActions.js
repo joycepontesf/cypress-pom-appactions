746bc118c15af262608008591473c41a0e846274
Joyce Pontes <joycepontesf@gmail.com>
2024-09-12 19:11:19 -0300

    Inclusão de novos comandos customizados

--- cypress/support/commands/checkoutActions.js
+++ cypress/support/commands/checkoutActions.js
@@ -1,37 +1,41 @@
 
 import { elements } from '../elements.js';
-import { email, password } from '../../fixtures/testData.json';
+import { email, password } from '../../fixtures/testData';
+
+Cypress.Commands.add('setEbacStoreVersionCookie', () => {
+    cy.setCookie('ebacStoreVersion', 'v2', { domain: 'lojaebac.ebaconline.art.br' });
+});
 
 Cypress.Commands.add('login', () => {
     cy.get(elements.btnProfile).click()
     cy.get(elements.txtEmail).type(email)
     cy.get(elements.txtPassword).eq(0).type(password)
     cy.get(elements.btnLogin).click()
-})
+});
 
 Cypress.Commands.add('addProduct', () => {
     cy.get(elements.product).eq(0).click()
     cy.get(elements.btnPurchase).click()
-})
+});
 
 Cypress.Commands.add('completeOrder', () => {
     cy.get(elements.btnContinueToCheckout).click()    
     cy.get(elements.btnCheckout).click()
     cy.wait(1000)
-})
+});
 
 Cypress.Commands.add('accessOrders', () => {
     cy.get(elements.btnOrder).click()
     cy.get(elements.processingOrders).click()
-})
+});
 
 Cypress.Commands.add('cancelOrder', () => {
     cy.get(elements.btnCancelOrder).click()
     cy.get(elements.btnConfirmCancelOrder).click()
 
     cy.get(elements.item).should('not.exist')
-})
+});
 
 Cypress.Commands.add('removeItem', () => {
     cy.get(elements.btnRemove).click()
-})
+});
