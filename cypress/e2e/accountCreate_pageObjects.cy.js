402828fd6de0470dbdd78e3759db80cd1fcefb8f
Joyce Pontes <joycepontesf@gmail.com>
2024-09-12 19:09:53 -0300

    Inclusão de novo teste

--- cypress/e2e/accountCreate_pageObjects.cy.js
+++ cypress/e2e/accountCreate_pageObjects.cy.js
@@ -1,25 +1,37 @@
 /// <reference types="cypress" />
 
-import newAccount from "../support/pages/login/index"
-import { firstName } from "../support/pages/login/index"
+import newAccount from "../support/pages/login";
+import { firstName } from "../support/pages/login";
 import { elements } from '../support/elements';
 
-describe('', () => {
+describe('Account creation flow', () => {
 
     beforeEach(() => {
-        cy.setCookie('ebacStoreVersion', 'v2', { domain: 'lojaebac.ebaconline.art.br' })
+        cy.setEbacStoreVersionCookie()
         cy.visit('/')
         newAccount.accessProfilePage()
-    })
+    });
 
     it('Create account successfully', () => {
         newAccount.fillNewAccountInformation()
-    
+
         cy.contains(elements.username, firstName).should('exist')
-    })
-    
+    });
+
+    it('Should show error messages when fields are empty', () => {
+        newAccount.fillNewAccountInformation({
+            firstNameInput: '',
+            lastNameInput: '',
+            phoneInput: '',
+            emailInput: '',
+            passwordInput: ''
+        });
+        
+        cy.get(elements.alertMsg).contains('Please enter firstName')
+    });
+
     afterEach(() => {
         cy.clearAllCookies
-    })
+    });
 
-})
+});
