f8a2280ae9bfc156fc4620dca77e42ce9e78b88a
Joyce Pontes <joycepontesf@gmail.com>
2024-09-19 17:17:50 -0300

    Adição de novos testes

--- cypress/e2e/accountCreate_pageObjects.cy.js
+++ cypress/e2e/accountCreate_pageObjects.cy.js
@@ -13,25 +13,75 @@ describe('Account creation flow', () => {
     });
 
     it('Create account successfully', () => {
-        newAccount.fillNewAccountInformation()
+        newAccount.accessSignUpPage()
+        newAccount.fillFirstName()
+        newAccount.fillLastName()
+        newAccount.fillPhone()
+        newAccount.fillEmail()
+        newAccount.fillPassword()
+        newAccount.createAccount()
 
         cy.contains(elements.username, firstName).should('exist')
     });
 
-    it('Should show error messages when fields are empty', () => {
-        newAccount.fillNewAccountInformation({
-            firstNameInput: '',
-            lastNameInput: '',
-            phoneInput: '',
-            emailInput: '',
-            passwordInput: ''
-        });
-        
+    it('Should show error messages when First Name is empty', () => {
+        newAccount.accessSignUpPage()
+        newAccount.fillFirstName({
+            firstNameInput: ''
+        })
+        newAccount.fillLastName()
+        newAccount.fillPhone()
+        newAccount.fillEmail()
+        newAccount.fillPassword()
+        newAccount.createAccount()
+
         cy.get(elements.alertMsg).contains('Please enter firstName')
     });
 
-    afterEach(() => {
-        cy.clearAllCookies
+    it('Should show error messages when Last Name is empty', () => {
+        newAccount.accessSignUpPage()
+        newAccount.fillFirstName()
+        newAccount.fillLastName({
+            lastNameInput: ''
+        })
+        newAccount.fillPhone()
+        newAccount.fillEmail()
+        newAccount.fillPassword()
+        newAccount.createAccount()
+
+        cy.get(elements.alertMsg).contains('Please enter lastName')
     });
 
+    it('Should show error messages when Email is empty', () => {
+        newAccount.accessSignUpPage()
+        newAccount.fillFirstName()
+        newAccount.fillLastName()
+        newAccount.fillPhone()
+        newAccount.fillEmail({
+            emailInput: ''
+        })
+        newAccount.fillPassword()
+        newAccount.createAccount()
+
+        cy.get(elements.alertMsg).contains('Please enter an email')
+    });
+
+    it('Should show error messages when fields are empty', () => {
+        newAccount.accessSignUpPage()
+        newAccount.fillFirstName()
+        newAccount.fillLastName()
+        newAccount.fillPhone()
+        newAccount.fillEmail()
+        newAccount.fillPassword({
+            passwordInput: ''
+        })
+        newAccount.createAccount()
+
+        cy.get(elements.alertMsg).contains('Please enter password')
+    });
+
+});
+
+afterEach(() => {
+    cy.clearAllCookies
 });
